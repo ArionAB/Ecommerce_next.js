4a8f5d7d2069ffebc39e3f77229ca94142cebae8
ArionAB <mihai.arion0@gmail.com>
2023-05-13 17:28:10 +0300

    added https

--- src/Utils/apiConfig.ts
+++ src/Utils/apiConfig.ts
@@ -1,4 +1,5 @@
-export var baseUrl = "http://henighonig-dev.eu-central-1.elasticbeanstalk.com/";
+export var baseUrl =
+  "https://henighonig-dev.eu-central-1.elasticbeanstalk.com/";
 // export var baseUrl = "https://localhost:5001/";
 
 export const resourceUrl = baseUrl + "Resources/";
