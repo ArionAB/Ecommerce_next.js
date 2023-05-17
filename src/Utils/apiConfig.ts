3cc28cfb1824c919440d7b275c052c25f98d5756
ArionAB <mihai.arion0@gmail.com>
2023-05-17 20:14:47 +0300

    small tweak to url

--- src/Utils/apiConfig.ts
+++ src/Utils/apiConfig.ts
@@ -1,5 +1,5 @@
 export var baseUrl =
-  "Beekeeping-env.eba-shhwzbqf.eu-central-1.elasticbeanstalk.com";
+  "Beekeeping-env.eba-shhwzbqf.eu-central-1.elasticbeanstalk.com/";
 // export var baseUrl = "https://localhost:5001/";
 
 export const resourceUrl = baseUrl + "Resources/";
