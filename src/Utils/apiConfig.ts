f720fa613a22ba65e0cf95d7daf38032ae7ea6ae
ArionAB <mihai.arion0@gmail.com>
2023-06-15 20:59:10 +0300

    added http:// to request

--- src/Utils/apiConfig.ts
+++ src/Utils/apiConfig.ts
@@ -1,4 +1,5 @@
-export var baseUrl = "ecommerce-dev-dev.eu-central-1.elasticbeanstalk.com/";
+export var baseUrl =
+  "http://ecommerce-dev-dev.eu-central-1.elasticbeanstalk.com/";
 // export var baseUrl = "https://localhost:5001/";
 
 export const resourceUrl = baseUrl + "Resources/";
