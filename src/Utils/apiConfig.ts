2fd7d0a0302668956c6ea18e534cd646d84ba381
ArionAB <mihai.arion0@gmail.com>
2023-06-15 20:48:18 +0300

    final url update for backend

--- src/Utils/apiConfig.ts
+++ src/Utils/apiConfig.ts
@@ -1,4 +1,4 @@
-export var baseUrl = "henig-honig-be.eu-central-1.elasticbeanstalk.com/";
+export var baseUrl = "ecommerce-dev-dev.eu-central-1.elasticbeanstalk.com/";
 // export var baseUrl = "https://localhost:5001/";
 
 export const resourceUrl = baseUrl + "Resources/";
